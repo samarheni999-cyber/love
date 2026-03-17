5bffc3323c41012e6a8712fd0af12cfc845168ec
samarheni999-cyber <samarheni999@gmail.com>
2026-03-17 14:31:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ noBtn.addEventListener("mouseover", () => {
     const maxX = window.innerWidth - noBtn.offsetWidth;
     const maxY = window.innerHeight - noBtn.offsetHeight;
 
-    // موقع عشوائي لكن قريب من الماوس
+    // موقع عشوائي جديد على كامل الشاشة
     let x = Math.random() * maxX;
     let y = Math.random() * maxY;
 
