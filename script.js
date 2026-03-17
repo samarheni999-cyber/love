b36f4b0282d6d18b45cf25a870ccfd233b485705
samarheni999-cyber <samarheni999@gmail.com>
2026-03-17 12:23:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+/* ===== NO BUTTON يهرب ===== */
+const noBtn = document.getElementById("no");
+
+noBtn.addEventListener("mouseover", () => {
+    const maxX = window.innerWidth - noBtn.offsetWidth;
+    const maxY = window.innerHeight - noBtn.offsetHeight;
+
+    let x = Math.random() * maxX;
+    let y = Math.random() * maxY;
+
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+});
+
+/* ===== YES BUTTON ===== */
+function showLove(){
+    document.getElementById("message").style.display = "block";
+    createHearts();
+}
+
+/* ===== HEARTS ===== */
+function createHearts(){
+    const container = document.getElementById("hearts-container");
+
+    for(let i=0; i<40; i++){
+        let heart = document.createElement("div");
+        heart.className = "heart";
+        heart.innerHTML = "❤️";
+
+        heart.style.left = Math.random() * window.innerWidth + "px";
+        heart.style.top = window.innerHeight + "px";
+        heart.style.fontSize = (15 + Math.random() * 40) + "px";
+
+        container.appendChild(heart);
+
+        setTimeout(()=>heart.remove(),3000);
+    }
+}
