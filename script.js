24c19e75a451b642858c2b758592bc19567017ed
samarheni999-cyber <samarheni999@gmail.com>
2026-03-17 14:18:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-/* ===== NO BUTTON يهرب ===== */
 const noBtn = document.getElementById("no");
 
 noBtn.addEventListener("mouseover", () => {
     const maxX = window.innerWidth - noBtn.offsetWidth;
     const maxY = window.innerHeight - noBtn.offsetHeight;
 
-    let x = Math.random() * maxX;
-    let y = Math.random() * maxY;
+    // الموقع الجديد قريب من الموقع الحالي
+    let currentX = parseFloat(noBtn.style.left || 0);
+    let currentY = parseFloat(noBtn.style.top || 0);
+
+    // Random shift صغير (-50px → +50px)
+    let x = currentX + (Math.random() * 100 - 50);
+    let y = currentY + (Math.random() * 100 - 50);
+
+    // ما يخرجش من الشاشة
+    x = Math.max(0, Math.min(maxX, x));
+    y = Math.max(0, Math.min(maxY, y));
 
     noBtn.style.left = x + "px";
     noBtn.style.top = y + "px";
 });
-
 /* ===== YES BUTTON ===== */
 function showLove(){
     document.getElementById("message").style.display = "block";
